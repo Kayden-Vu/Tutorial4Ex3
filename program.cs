6f2c2c589a6700eae02617c1a8d236a40064e44e
Kayden VU <12300952@students.koi.edu.au>
2024-08-11 23:23:35 +1000

    Add Console.WriteLine

--- program.cs
+++ program.cs
@@ -2,7 +2,8 @@ class program
 {
     public static void Main (string[] args)
     {
-        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World, My name is Kayden");
+        Console.WriteLine("This is how I practice using Github");
         Console.Read();
     }
 }
